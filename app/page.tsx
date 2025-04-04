8a4e7d204b44ee15a8fbf583ebb46c1d5e936839
Forrest Weigel <eldrxofficial@gmail.com>
2025-04-04 15:40:59 -0500

    update images

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
                 style={{ backgroundImage: 'url(background.jpg)' }}
                 className="w-screen h-screen bg-cover bg-center flex flex-col p-10"
             >
-                <Image src="/logo-full.png" alt="Armada Nexus Logo" width="500" height="300" className="m-auto" />
+                <Image src="/logo-full.svg" alt="Armada Nexus Logo" width="500" height="300" className="m-auto" />
             </div>
         </>
     )
