1fdeef036c77f93cec278c4195dc6678531379c4
Forrest Weigel <eldrxofficial@gmail.com>
2025-04-04 18:39:22 -0500

    Landing page auth test

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image"
+import { auth0 } from "@/lib/auth0"
 
-export default function Home() {
+export default async function Home() {
+    const session = await auth0.getSession()
+
     return (
         <>
             <div
                 style={{ backgroundImage: 'url(background.jpg)' }}
-                className="w-screen h-screen bg-cover bg-center flex flex-col p-10"
+                className="w-screen h-screen bg-cover bg-center flex flex-col p-10 justify-center items-center gap-4"
             >
-                <Image src="/logo-full.svg" alt="Armada Nexus Logo" width="500" height="300" className="m-auto" />
+                <Image src="/logo-full.svg" alt="Armada Nexus Logo" width="500" height="300" />
+                <h1 className="text-white text-4xl">{session ? session.user.name : 'Please Log In'}</h1>
             </div>
         </>
     )
