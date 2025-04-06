86858e83a7ea094f42f01393871722908fb901f5
Forrest Weigel <eldrxofficial@gmail.com>
2025-04-06 14:23:18 -0500

    mobile nav menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,27 @@
 import type { Metadata } from "next"
 import Navigation from "@/components/navigation"
+import { auth0 } from "@/lib/auth0"
 import "./globals.css"
 
 export const metadata: Metadata = {
     title: "Armada Nexus",
 }
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode
 }>) {
+    const session = await auth0.getSession()
+
     return (
         <html lang="en" className="dark">
             <body
-                className="bg-center bg-cover "
+                className="bg-center bg-cover h-screen"
                 style={{ backgroundImage: 'url(background.jpg)' }}
             >
-                <Navigation />
-                <div className="ml-16 flex flex-col items-center h-screen p-10">
+                <Navigation session={session} />
+                <div className="ml-16 flex flex-col items-center h-full p-10 pointer-coarse:ml-0 pointer-coarse:pt-24">
                     {children}
                 </div>
             </body>
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,10 +1,13 @@
-import { User, Settings } from "lucide-react"
+"use client"
+import { useState } from "react"
+import { User, Settings, Menu, X } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-import { auth0 } from "@/lib/auth0"
 import StarForge from "@/components/StarForge"
 import Nexus from "@/components/Nexus"
 import { FaSteam } from 'react-icons/fa';
+import { SessionData } from "@auth0/nextjs-auth0/types"
+import { Button } from "./ui/button"
 
 interface NavItemProps {
     label: string
@@ -31,9 +34,13 @@ const testNavItems: NavItemProps[] = [
     }
 ]
 
-const NavItem = (props: NavItemProps) => {
+const NavItem = ({ props, onClick }: { props: NavItemProps, onClick: () => void }) => {
     return (
-        <Link key={props.href} href={props.href} className="flex hover:bg-foreground/10 py-3">
+        <Link
+            href={props.href}
+            onClick={onClick}
+            className="flex hover:bg-foreground/10 py-3"
+        >
             <div className="min-w-16 flex justify-center items-center">
                 {props.imageSrc !== undefined
                     ? <Image src={props.imageSrc} alt={props.imageSrc} width="32" height="32" className="rounded-full" />
@@ -46,32 +53,66 @@ const NavItem = (props: NavItemProps) => {
     )
 }
 
-export default async function Navigation() {
-    const session = await auth0.getSession()
+export default function Navigation({ session }: { session: SessionData | null }) {
+    const [isMobileNavMenuOpen, setIsMobileNavMenuOpen] = useState<boolean>(false)
 
     return (
         <div className="group">
-            <div className="fixed h-screen w-screen z-40 pointer-events-none group-hover:backdrop-blur-sm group-hover:bg-black/20 transition-bg tranisition-backdrop duration-200" />
+            <div
+                className={
+                    `fixed top-0 bottom-0 left-0 right-0 z-30 pointer-events-none transition-bg tranisition-backdrop duration-200
+                    group-hover:backdrop-blur-smgroup-hover:bg-black/20
+                    ${isMobileNavMenuOpen ? 'backdrop-blur-sm bg-black/20 pointer-events-auto' : ''}
+                `}
+                onClick={() => setIsMobileNavMenuOpen(!isMobileNavMenuOpen)}
+            />
 
-            <div className="bg-background w-16 z-50 fixed top-0 left-0 h-screen hover:w-80 transition-w duration-200 overflow-hidden flex flex-col py-6">
+            <div
+                className="hidden fixed right-0 left-0 top-0 pointer-coarse:flex bg-background border h-16 px-3 items-center z-40"
+            >
+                {isMobileNavMenuOpen
+                    ? <X className="w-5 h-5 m-2" onClick={() => setIsMobileNavMenuOpen(!isMobileNavMenuOpen)} />
+                    : <Menu className="w-5 h-5 m-2" onClick={() => setIsMobileNavMenuOpen(!isMobileNavMenuOpen)} />
+                }
+            </div>
+
+
+            <div
+                className={
+                    `bg-background w-16 z-50 fixed top-0 left-0 bottom-0 transition-w duration-200 overflow-hidden flex flex-col py-6
+                    hover:w-80
+                    pointer-coarse:-translate-x-80 pointer-coarse:w-80 pointer-coarse:top-16
+                    ${isMobileNavMenuOpen ? 'pointer-coarse:translate-x-0' : ''}
+                `}
+            >
                 {testNavItems.map((navItem) => (
-                    NavItem(navItem)
+                    <NavItem
+                        key={navItem.href + navItem.label}
+                        props={navItem}
+                        onClick={() => setIsMobileNavMenuOpen(!isMobileNavMenuOpen)}
+                    />
                 ))}
 
                 <div className="flex-grow" />
 
-                {NavItem({
-                    label: session?.user.name ? session.user.name : "Log in",
-                    href: session ? "/user" : "/auth/login",
-                    icon: <User className="w-5 h-5" />,
-                    imageSrc: session ? session.user.picture : undefined
-                })}
+                <NavItem
+                    props={{
+                        label: session?.user.name ? session.user.name : "Log in",
+                        href: session ? "/user" : "/auth/login",
+                        icon: <User className="w-5 h-5" />,
+                        imageSrc: session ? session.user.picture : undefined
+                    }}
+                    onClick={() => setIsMobileNavMenuOpen(!isMobileNavMenuOpen)}
+                />
 
-                {NavItem({
-                    label: "Settings",
-                    href: "/settings",
-                    icon: <Settings className="w-5 h-5" />
-                })}
+                <NavItem
+                    props={{
+                        label: "Settings",
+                        href: "/settings",
+                        icon: <Settings className="w-5 h-5" />
+                    }}
+                    onClick={() => setIsMobileNavMenuOpen(!isMobileNavMenuOpen)}
+                />
             </div>
         </div>
     );
