1ae3e25726dbef9ebacd47d995fbcd2bdd74e2b6
Forrest Weigel <eldrxofficial@gmail.com>
2025-04-06 12:05:49 -0500

    build fixes

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,16 +1,15 @@
-import Image from "next/image"
-import { auth0 } from "@/lib/auth0"
+// import { auth0 } from "@/lib/auth0"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 
 export default async function User() {
-    const session = await auth0.getSession()
+    // const session = await auth0.getSession()
 
     return (
         <div className="max-w-128 mx-auto flex flex-col gap-2">
-            <a href="/auth/logout">
+            <Link href="/auth/logout">
                 <Button>Logout</Button>
-            </a>
+            </Link>
         </div>
     )
 }
